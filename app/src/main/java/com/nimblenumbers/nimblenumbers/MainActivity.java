1508d61e68955172cd2a30f815b154195bc5bb89
Dylan Budgen <15916056+dylanbudgen@users.noreply.github.com>
2017-07-15 16:20:44 +0100

    First commit

--- app/src/main/java/com/nimblenumbers/nimblenumbers/MainActivity.java
+++ app/src/main/java/com/nimblenumbers/nimblenumbers/MainActivity.java
@@ -1,7 +1,10 @@
 package com.nimblenumbers.nimblenumbers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void startGame(View view) {
+
+        Log.d("DEBUG", "000P Button pressed ");
+
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
     }
 }

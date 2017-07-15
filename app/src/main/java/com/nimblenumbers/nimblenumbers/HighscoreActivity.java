fb1d8659c4c3ab50aba6ff1d7da5ff1b490aa365
Sophie <sophie0z@yahoo.com>
2017-07-15 22:19:48 +0100

    Main screen buttons and preferences

---
+++ app/src/main/java/com/nimblenumbers/nimblenumbers/HighscoreActivity.java
@@ -0,0 +1,8 @@
+package com.nimblenumbers.nimblenumbers;
+
+/**
+ * Created by Sophie on 7/15/2017.
+ */
+
+public class HighscoreActivity {
+}
--- app/src/main/java/com/nimblenumbers/nimblenumbers/MainActivity.java
+++ app/src/main/java/com/nimblenumbers/nimblenumbers/MainActivity.java
@@ -1,21 +1,54 @@
 package com.nimblenumbers.nimblenumbers;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean sound = sharedPref.getBoolean("sound", true);
+        Button highscore = (Button) findViewById(R.id.button_highscore);
+        if (sharedPref.getInt("highscore", 0) == 0) {
+            highscore.setEnabled(false);
+            setInitialPreferences(sharedPref);
+        }
+        else{
+            highscore.setEnabled(true);
+
+        }
     }
 
+    private void setInitialPreferences(SharedPreferences sharedPref){
+        int avgAcc = sharedPref.getInt("avgAcc", 0);
+        int totalQuestions = sharedPref.getInt("totalQuestions", 0);
+        double avgTime = sharedPref.getInt("avgTime", 0);
+        int totalCorrect = sharedPref.getInt("totalCorrect", 0);
+        int totalInorrect = sharedPref.getInt("totalInorrect", 0);
+        int currentLevel = sharedPref.getInt("currentLevel", 0);
 
-
+    }
+/*
+    public void setSound(View view, SharedPreferences sharedPref){
+        if sharedPref.getBoolean("sound", true)
+    }*/
+    public void seeHighscore(View view){
+        Log.d("DEBUG", "000P Highscore pressed ");
+        Intent intent = new Intent(this, HighscoreActivity.class);
+        startActivity(intent);
+    }
     public void startGame(View view) {
 
         Log.d("DEBUG", "000P Button pressed ");
